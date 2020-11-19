0af3cce4a78b52332ac38841c4b352b6086a95db
Victor Azevedo <victorazevedomagalhaes@gmail.com>
2020-11-19 11:22:51 -0300

    Projeto Finalizado

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default{
+    title: 'light',
+
+    colors:{
+        primary: '#C62E65',
+        secondary: '#d63af9',
+
+        background: '#f5f5f5',
+        text: '#333'
+    },
+}
